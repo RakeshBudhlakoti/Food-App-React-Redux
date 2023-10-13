e75e0c2fbe740534622a8c07b0c3138dba555c0f
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-10-13 16:58:21 +0530

    New Feature Added

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { CDN_URL } from '../utils/constants';
+import React from "react";
+import { CDN_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 
 const RestaurantCard = ({ resData }) => {
@@ -19,31 +19,36 @@ const RestaurantCard = ({ resData }) => {
     sla,
   } = resData.info || {};
 
-  const truncatedCuisines = cuisines.join(', ').substring(0, 20) + '...';
+  const truncatedCuisines = cuisines.join(", ").substring(0, 20) + "...";
 
   return (
     <div className="card">
-      <img className="cardImg" src={`${CDN_URL}${cloudinaryImageId}`} alt="Restaurant Image" />
-      <div className="container1">
-        <h4>
-          <b>{name}</b>
-        </h4>
-        <h5>
-          {`${areaName} (${sla.slaString})`}
-        </h5>
-        <h5>{truncatedCuisines}</h5>
-        <h5>{costForTwo}</h5>
-        <h5>
-          <span className="rating">{avgRating}</span> ({totalRatingsString})
-        </h5>
+      <Link to={"/restaurants/" + id}>
+        <div className="cardBody">
+          <img
+            className="cardImg"
+            src={`${CDN_URL}${cloudinaryImageId}`}
+            alt="Restaurant Image"
+          />
+
+          <h4>
+            <b>{name}</b>
+          </h4>
+          <h5>{`${areaName} (${sla.slaString})`}</h5>
+          <h5>{truncatedCuisines}</h5>
+          <h5>{costForTwo}</h5>
+          <h5>
+            <span className="rating">{avgRating}</span> ({totalRatingsString})
+          </h5>
+          {/* 
         <div className="orderNow-button">
           
         <Link to={"/restaurants/" + id} >
           <button>Order Now</button>
           </Link>
-          </div>
-          
-      </div>
+          </div>*/}
+        </div>
+      </Link>
     </div>
   );
 };
