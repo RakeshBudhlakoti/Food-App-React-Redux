cdeaf3a26b89bbec6bf9b39d4bf7b5d0ef04b90c
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2024-02-11 12:07:02 +0530

    Proxy remove

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,18 +1,18 @@
-export const MENU_API = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5017782&lng=77.18794419999999&restaurantId=";
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5017782&lng=77.18794419999999&restaurantId=";
 
-export const CDN_URL = "https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const RESTRA_LIST_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?";
+export const RESTRA_LIST_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?";
 
-export const RESTRA_CDN_URL = "https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+export const RESTRA_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
-export const LOGO_URL = "https://corsproxy.io/?https://cdn.worldvectorlogo.com/logos/swiggy-1.svg";
+export const LOGO_URL = "https://cdn.worldvectorlogo.com/logos/swiggy-1.svg";
 
-export const RESTRA_DETAIL_PAGE = "https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_18,h_18/";
+export const RESTRA_DETAIL_PAGE = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_18,h_18/";
 
-export const AUTOCOMPLETE_SEARCH_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/misc/place-autocomplete?input=";
+export const AUTOCOMPLETE_SEARCH_URL = "https://www.swiggy.com/dapi/misc/place-autocomplete?input=";
 
-export const AREA_RESTAURANTS_FETCH_URL = "https://corsproxy.io/?https://www.swiggy.com/dapi/misc/address-recommend?place_id="
+export const AREA_RESTAURANTS_FETCH_URL = "https://www.swiggy.com/dapi/misc/address-recommend?place_id="
 
-export const GET_ADDRESS_BY_LAT_LNG = "https://corsproxy.io/?https://www.swiggy.com/dapi/misc/address-recommend?latlng="
+export const GET_ADDRESS_BY_LAT_LNG = "https://www.swiggy.com/dapi/misc/address-recommend?latlng="
 
